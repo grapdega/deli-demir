323a4adbd123f1250f012f3ea5eb487404a78cda
aliriza <aliriza.keskin@pardus.org.tr>
2025-04-12 21:53:37 +0300

    mob bug fix

--- sahneler/karakter/mob_base.gd
+++ sahneler/karakter/mob_base.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 @export var heal = 100
 
-@export var speed = 80
+@export var speed = 3000
+
+var can_move = true
 
 var player: CharacterBody2D
 @export var sprite: Texture
@@ -18,9 +20,8 @@ func _process(delta: float) -> void:
 	if player:
 		global_position.y = max(player.global_position.y, global_position.y)
 		var diff = global_position.x - player.global_position.x
-		if abs(diff) > 31:
-			if diff > 0:
-				global_position.x -= delta*speed
-			elif diff < 0:
-				global_position.x += delta*speed
- 
+		if diff > 0:
+			velocity = Vector2(delta*speed, 0) * -1
+		elif diff < 0:
+			velocity = Vector2(delta*speed, 0)
+		move_and_slide()
