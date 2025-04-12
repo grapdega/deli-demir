3aeb139c7e6e3e8bd2d40a1bb8c548eb3758ccb8
aliriza <aliriza.keskin@pardus.org.tr>
2025-04-12 20:23:01 +0300

    lvl2

--- sahneler/bolumler/bolum_2.gd
+++ sahneler/bolumler/bolum_2.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	$zemin.global_position.x = $Oyuncu.global_position.x
+	$Oyuncu/kamera.limit_bottom = 238
+	$Oyuncu/kamera.limit_top = 0
+	$Oyuncu/kamera.position.y = -66
+	
 	
